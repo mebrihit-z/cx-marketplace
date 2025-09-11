21addc4c7f86a8bf5bd2e51fb02c32497c638295
mebrihit-z <mebrihit@cxstudios.io>
2025-09-11 16:18:28 -0400

    moving the table to it's comp

---
+++ src/components/comparisonTable.tsx
@@ -0,0 +1,221 @@
+// Image assets for comparison table
+const imgImage46 = "images/comparison-table-bg-image.svg";
+const imgLine7 = "icons/line-image.svg";
+const imgLine15 = "icons/line-white-image.png";
+const imgMaterialSymbolsCheckRounded = "icons/green-check-icon.svg";
+const xIcon = "icons/x-icon.svg";
+
+export default function ComparisonTable() {
+  return (
+    <div className="bg-[#020d10] box-border content-stretch flex flex-col gap-12 items-start justify-start px-0 py-16 relative size-full" data-name="Section Container" data-node-id="8306:40159">
+      <div className="absolute flex h-[1054px] items-center justify-center left-[-35px] top-0 w-[1581px] hidden lg:block">
+        <div className="flex-none rotate-[180deg]">
+          <div className="bg-center bg-cover bg-no-repeat h-[1054px] opacity-70 w-[1581px]" data-name="image 46" data-node-id="8306:40160" style={{ backgroundImage: `url('${imgImage46}')` }} />
+        </div>
+      </div>
+      <div className="box-border content-stretch flex items-start justify-start px-4 sm:px-8 lg:px-32 py-0 relative shrink-0 w-full" data-name="Header" data-node-id="8306:40161">
+        <div className="content-stretch flex flex-col items-start justify-center relative shrink-0 w-full lg:w-[480px]" data-name="Title Container" data-node-id="8306:40162">
+          <div className="font-['Inter:Semi_Bold',_sans-serif] font-semibold leading-[0] not-italic relative shrink-0 text-[32px] sm:text-[40px] lg:text-[48px] text-white w-full" data-node-id="8306:40163">
+            <p className="leading-[40px] sm:leading-[48px] lg:leading-[56px]">Compare packages</p>
+          </div>
+        </div>
+      </div>
+      <div className="box-border content-stretch flex gap-[120px] items-center justify-center overflow-x-auto px-4 sm:px-8 lg:px-32 py-0 relative shrink-0 w-full" data-name="Container" data-node-id="8306:40164">
+        <div className="grid-cols-[max-content] grid-rows-[max-content] inline-grid leading-[0] place-items-start relative shrink-0 min-w-[1200px]" data-name="Table Container" data-node-id="8306:40165">
+          <div className="[grid-area:1_/_1] bg-[#737780] h-[662px] ml-[928px] mt-0 opacity-[0.15] relative rounded-br-[16px] rounded-tr-[16px] w-[309px]" data-node-id="8306:40166">
+            <div aria-hidden="true" className="absolute border border-solid border-white inset-0 pointer-events-none rounded-br-[16px] rounded-tr-[16px]" />
+          </div>
+          <div className="[grid-area:1_/_1] bg-[#737780] h-[580px] ml-0 mt-[82px] opacity-30 relative rounded-bl-[16px] rounded-tl-[16px] w-[309px]" data-node-id="8306:40167">
+            <div aria-hidden="true" className="absolute border border-solid border-white inset-0 pointer-events-none rounded-bl-[16px] rounded-tl-[16px]" />
+          </div>
+          <div className="[grid-area:1_/_1] bg-[#737780] h-[662px] ml-[308px] mt-0 opacity-[0.15] relative rounded-tl-[16px] w-[311px]" data-node-id="8306:40168">
+            <div aria-hidden="true" className="absolute border-[0px_1px_1px] border-solid border-white inset-0 pointer-events-none rounded-tl-[16px]" />
+          </div>
+          <div className="[grid-area:1_/_1] bg-[#737780] h-[82px] ml-[308.5px] mt-0 opacity-30 relative rounded-tl-[16px] rounded-tr-[16px] w-[929px]" data-node-id="8306:40169">
+            <div aria-hidden="true" className="absolute border border-solid border-white inset-[-0.5px] pointer-events-none rounded-tl-[16.5px] rounded-tr-[16.5px]" />
+          </div>
+          <div className="[grid-area:1_/_1] bg-[#737780] h-[662px] ml-[619px] mt-0 opacity-[0.15] relative w-[309px]" data-node-id="8306:40170">
+            <div aria-hidden="true" className="absolute border-[0px_1px_1px] border-solid border-white inset-0 pointer-events-none" />
+          </div>
+          <div className="[grid-area:1_/_1] ml-[1066.5px] mt-[606px] relative size-8" data-name="material-symbols:check-rounded" data-node-id="8306:40171">
+            <img alt="" className="block max-w-none size-full" src={imgMaterialSymbolsCheckRounded} />
+          </div>
+          <div className="[grid-area:1_/_1] flex flex-col font-['Inter:Semi_Bold',_sans-serif] font-semibold justify-center ml-[464px] mt-[41px] not-italic relative text-[32px] text-center text-nowrap text-white tracking-[-1px] translate-x-[-50%] translate-y-[-50%]" data-node-id="8306:40175">
+            <p className="leading-[40px] whitespace-pre">Basic</p>
+          </div>
+          <div className="[grid-area:1_/_1] flex flex-col font-['Inter:Semi_Bold',_sans-serif] font-semibold justify-center ml-[774.5px] mt-[41px] not-italic relative text-[32px] text-center text-nowrap text-white tracking-[-1px] translate-x-[-50%] translate-y-[-50%]" data-node-id="8306:40176">
+            <p className="leading-[40px] whitespace-pre">Pro</p>
+          </div>
+          <div className="[grid-area:1_/_1] flex flex-col font-['Inter:Semi_Bold',_sans-serif] font-semibold justify-center ml-[1084.5px] mt-[41px] not-italic relative text-[32px] text-center text-nowrap text-white tracking-[-1px] translate-x-[-50%] translate-y-[-50%]" data-node-id="8306:40177">
+            <p className="leading-[40px] whitespace-pre">Premium</p>
+          </div>
+          <div className="[grid-area:1_/_1] h-0 ml-[309px] mt-[154px] relative w-[928px]" data-node-id="8306:40178">
+            <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+              <img alt="" className="block max-w-none size-full" src={imgLine7} />
+            </div>
+          </div>
+          <div className="[grid-area:1_/_1] h-0 ml-[309px] mt-[226px] relative w-[928px]" data-node-id="8306:40179">
+            <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+              <img alt="" className="block max-w-none size-full" src={imgLine7} />
+            </div>
+          </div>
+          <div className="[grid-area:1_/_1] h-0 ml-[309px] mt-[298px] relative w-[928px]" data-node-id="8306:40180">
+            <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+              <img alt="" className="block max-w-none size-full" src={imgLine7} />
+            </div>
+          </div>
+          <div className="[grid-area:1_/_1] h-0 ml-[309px] mt-[370px] relative w-[928px]" data-node-id="8306:40181">
+            <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+              <img alt="" className="block max-w-none size-full" src={imgLine7} />
+            </div>
+          </div>
+          <div className="[grid-area:1_/_1] h-0 ml-[309px] mt-[442px] relative w-[928px]" data-node-id="8306:40182">
+            <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+              <img alt="" className="block max-w-none size-full" src={imgLine7} />
+            </div>
+          </div>
+          <div className="[grid-area:1_/_1] h-0 ml-[309px] mt-[514px] relative w-[928px]" data-node-id="8306:40183">
+            <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+              <img alt="" className="block max-w-none size-full" src={imgLine7} />
+            </div>
+          </div>
+          <div className="[grid-area:1_/_1] h-0 ml-[309px] mt-[586px] relative w-[928px]" data-node-id="8306:40184">
+            <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+              <img alt="" className="block max-w-none size-full" src={imgLine7} />
+            </div>
+          </div>
+          <div className="[grid-area:1_/_1] grid-cols-[max-content] grid-rows-[max-content] inline-grid ml-px mt-[106px] place-items-start relative" data-node-id="8306:40185">
+            <div className="[grid-area:1_/_1] h-0 ml-0 mt-12 relative w-[308px]" data-node-id="8306:40187">
+              <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+                <img alt="" className="block max-w-none size-full" src={imgLine15} />
+              </div>
+            </div>
+            <div className="[grid-area:1_/_1] h-0 ml-0 mt-[120px] relative w-[308px]" data-node-id="8306:40188">
+              <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+                <img alt="" className="block max-w-none size-full" src={imgLine15} />
+              </div>
+            </div>
+            <div className="[grid-area:1_/_1] h-0 ml-0 mt-48 relative w-[308px]" data-node-id="8306:40189">
+              <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+                <img alt="" className="block max-w-none size-full" src={imgLine15} />
+              </div>
+            </div>
+            <div className="[grid-area:1_/_1] h-0 ml-0 mt-[264px] relative w-[308px]" data-node-id="8306:40190">
+              <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+                <img alt="" className="block max-w-none size-full" src={imgLine15} />
+              </div>
+            </div>
+            <div className="[grid-area:1_/_1] h-0 ml-0 mt-[336px] relative w-[308px]" data-node-id="8306:40191">
+              <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+                <img alt="" className="block max-w-none size-full" src={imgLine15} />
+              </div>
+            </div>
+            <div className="[grid-area:1_/_1] h-0 ml-0 mt-[408px] relative w-[308px]" data-node-id="8306:40192">
+              <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+                <img alt="" className="block max-w-none size-full" src={imgLine15} />
+              </div>
+            </div>
+            <div className="[grid-area:1_/_1] h-0 ml-0 mt-[480px] relative w-[308px]" data-node-id="8306:40193">
+              <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+                <img alt="" className="block max-w-none size-full" src={imgLine15} />
+              </div>
+            </div>
+            <div className="[grid-area:1_/_1] font-['Inter:Semi_Bold',_sans-serif] font-semibold ml-[25px] mt-0 not-italic relative text-[20px] text-nowrap text-white" data-node-id="8306:40195">
+              <p className="leading-[24px] whitespace-pre">Total pages</p>
+            </div>
+            <div className="[grid-area:1_/_1] font-['Inter:Semi_Bold',_sans-serif] font-semibold ml-[25px] mt-[72px] not-italic relative text-[20px] text-nowrap text-white" data-node-id="8306:40196">
+              <p className="leading-[24px] whitespace-pre">Authorized users</p>
+            </div>
+            <div className="[grid-area:1_/_1] font-['Inter:Semi_Bold',_sans-serif] font-semibold ml-[25px] mt-36 not-italic relative text-[20px] text-nowrap text-white" data-node-id="8306:40197">
+              <p className="leading-[24px] whitespace-pre">Customization options</p>
+            </div>
+            <div className="[grid-area:1_/_1] font-['Inter:Semi_Bold',_sans-serif] font-semibold ml-[25px] mt-[216px] not-italic relative text-[20px] text-nowrap text-white" data-node-id="8306:40198">
+              <p className="leading-[24px] whitespace-pre">SEO optimization</p>
+            </div>
+            <div className="[grid-area:1_/_1] font-['Inter:Semi_Bold',_sans-serif] font-semibold ml-[25px] mt-72 not-italic relative text-[20px] text-nowrap text-white" data-node-id="8306:40199">
+              <p className="leading-[24px] whitespace-pre">CRM integration</p>
+            </div>
+            <div className="[grid-area:1_/_1] font-['Inter:Semi_Bold',_sans-serif] font-semibold ml-[25px] mt-[360px] not-italic relative text-[20px] text-nowrap text-white" data-node-id="8306:40200">
+              <p className="leading-[24px] whitespace-pre">Analytics dashboard</p>
+            </div>
+            <div className="[grid-area:1_/_1] font-['Inter:Semi_Bold',_sans-serif] font-semibold ml-[25px] mt-[432px] not-italic relative text-[20px] text-nowrap text-white" data-node-id="8306:40201">
+              <p className="leading-[24px] whitespace-pre">Web/graphic designer</p>
+            </div>
+            <div className="[grid-area:1_/_1] font-['Inter:Semi_Bold',_sans-serif] font-semibold ml-[25px] mt-[504px] not-italic relative text-[20px] text-nowrap text-white" data-node-id="8306:40202">
+              <p className="leading-[24px] whitespace-pre">Custom template</p>
+            </div>
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[463.5px] mt-[108px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40203">
+            <p className="leading-[20px] whitespace-pre">Up to 100</p>
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[463.5px] mt-[180px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40204">
+            <p className="leading-[20px] whitespace-pre">Up to 10</p>
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[463.5px] mt-[252px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40205">
+            <p className="leading-[20px] whitespace-pre">Limited</p>
+          </div>
+          <div className="[grid-area:1_/_1] ml-[447.5px] mt-[534px] relative size-8" data-name="material-symbols:close-rounded" data-node-id="8310:44794">
+            <img alt="" className="block max-w-none size-full" src={xIcon} />
+          </div>
+          <div className="[grid-area:1_/_1] ml-[447.5px] mt-[390px] relative size-8" data-name="material-symbols:close-rounded" data-node-id="8306:40208">
+            <img alt="" className="block max-w-none size-full" src={xIcon} />
+          </div>
+          <div className="[grid-area:1_/_1] ml-[447.5px] mt-[318px] relative size-8" data-name="material-symbols:close-rounded" data-node-id="8306:40210">
+            <img alt="" className="block max-w-none size-full" src={xIcon} />
+          </div>
+          <div className="[grid-area:1_/_1] ml-[447.5px] mt-[606px] relative size-8" data-name="material-symbols:close-rounded" data-node-id="8306:40212">
+            <img alt="" className="block max-w-none size-full" src={xIcon} />
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[463px] mt-[468px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8310:44754">
+            <p className="leading-[20px] whitespace-pre">Basic</p>
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[773.5px] mt-[108px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40216">
+            <p className="leading-[20px] whitespace-pre">Up to 500</p>
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[773.5px] mt-[180px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40217">
+            <p className="leading-[20px] whitespace-pre">Up to 20</p>
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[773.5px] mt-[252px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40218">
+            <p className="leading-[20px] whitespace-pre">Advanced</p>
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[774px] mt-[324px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40219">
+            <p className="leading-[20px] whitespace-pre">Basic</p>
+          </div>
+          <div className="[grid-area:1_/_1] ml-[757.5px] mt-[534px] relative size-8" data-name="material-symbols:check-rounded" data-node-id="8306:40220">
+            <img alt="" className="block max-w-none size-full" src={imgMaterialSymbolsCheckRounded} />
+          </div>
+          <div className="[grid-area:1_/_1] ml-[757.5px] mt-[606px] relative size-8" data-name="material-symbols:close-rounded" data-node-id="8306:40222">
+            <img alt="" className="block max-w-none size-full" src={xIcon} />
+          </div>
+          <div className="[grid-area:1_/_1] ml-[757.5px] mt-[390px] relative size-8" data-name="material-symbols:close-rounded" data-node-id="8306:40224">
+            <img alt="" className="block max-w-none size-full" src={xIcon} />
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[773.5px] mt-[468px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40226">
+            <p className="leading-[20px] whitespace-pre">Advanced</p>
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[1082.5px] mt-[108px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40227">
+            <p className="leading-[20px] whitespace-pre">Up to 1000</p>
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[1082px] mt-[180px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40228">
+            <p className="leading-[20px] whitespace-pre">Up to 30</p>
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[1082.5px] mt-[252px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40229">
+            <p className="leading-[20px] whitespace-pre">Full</p>
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[1082.5px] mt-[324px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40230">
+            <p className="leading-[20px] whitespace-pre">Advanced</p>
+          </div>
+          <div className="[grid-area:1_/_1] ml-[1066.5px] mt-[389px] relative size-8" data-name="material-symbols:check-rounded" data-node-id="8306:40231">
+            <img alt="" className="block max-w-none size-full" src={imgMaterialSymbolsCheckRounded} />
+          </div>
+          <div className="[grid-area:1_/_1] ml-[1066.5px] mt-[534px] relative size-8" data-name="material-symbols:check-rounded" data-node-id="8306:40233">
+            <img alt="" className="block max-w-none size-full" src={imgMaterialSymbolsCheckRounded} />
+          </div>
+          <div className="[grid-area:1_/_1] font-['Inter:regular',_sans-serif] ml-[1082px] mt-[468px] not-italic relative text-[16px] text-center text-nowrap text-white translate-x-[-50%]" data-node-id="8306:40235">
+            <p className="leading-[20px] whitespace-pre">Intelligent</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
