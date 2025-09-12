a99f1699fb6bc7f8a5dd140094dad3594b0ac8a1
mebrihit-z <mebrihit@cxstudios.io>
2025-09-12 16:58:47 -0400

    Testimonials added

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,13 +1,17 @@
 import GrowthSolutions from './growth-solutions';
 import IntegrationsSection from './integrations-section';
 import LeadOrganization from './lead-organization';
+import Testimonials from './testimonials';
+
 export default function Contact() {
     return (
       <>
         <h1>Contact</h1>
+        <Testimonials />
         <GrowthSolutions />
         <LeadOrganization />
         <IntegrationsSection />
+        <Testimonials />
       </>
     );
 }
---
+++ src/components/testimonials.tsx
@@ -0,0 +1,204 @@
+const imgVector1 = "http://localhost:3845/assets/e886a86c6efbc9998c5cc527827c1cd38babd8d9.png";
+const imgVector5 = "http://localhost:3845/assets/15201e9ad1d338c03541a332b177176dcb60e6ae.png";
+const imgVector7 = "http://localhost:3845/assets/0fa4fe5588eba53495dcb332db3bf9d16f1919e1.png";
+const imgVector11 = "http://localhost:3845/assets/21babd652d9d02544c75474aea543558d0b2f749.png";
+const imgImage = "http://localhost:3845/assets/a21891a886021676f13d1c6bd5f2d694186058c1.png";
+const imgImage1 = "http://localhost:3845/assets/4c2f22466556e368e25b08f2e739109fec03f7ab.png";
+const imgImage2 = "http://localhost:3845/assets/5254c5e2d2c6f201446497712e539eaa1a0cf65d.png";
+const imgImage3 = "http://localhost:3845/assets/7e34a15877fc7b69dc78323bd4ba3a4bfab2b615.png";
+const imgImage4 = "http://localhost:3845/assets/8f8d8aa2c45d8aa76b2d456dec0d773ef7a7818a.png";
+const imgImage5 = "http://localhost:3845/assets/02c22f5f6b86947ee9c5a8836cac206c578275fc.png";
+const imgVector = "http://localhost:3845/assets/9787a9213c4b521b0b01f1ba7e96a52d7f944181.svg";
+const imgVector2 = "http://localhost:3845/assets/6348ba033db2e59b006eed51e647d6836cfa44fc.svg";
+const imgVector3 = "http://localhost:3845/assets/0416f3a5da6ad6c577b19f3e1bbdc2af3228e9.svg";
+const imgVector4 = "http://localhost:3845/assets/f5c255f8593ad7418ec22c4886f9d92f868b153d.svg";
+const imgVector6 = "http://localhost:3845/assets/200ac77448e083c57a8bb774ad94c746ea5cdf85.svg";
+const imgVector8 = "http://localhost:3845/assets/5acc0382bf0959232afb36d052e64dfdca3ca4fd.svg";
+const imgVector9 = "http://localhost:3845/assets/b48c2791622270a298cdfe1418983d738f4c8573.svg";
+const imgVector10 = "http://localhost:3845/assets/ca2aedfdb68e00dbc7aa55db10f70bd9629fd573.svg";
+
+export default function Testimonials() {
+  return (
+    <div className="bg-[#041117] box-border content-stretch flex flex-col gap-12 items-center justify-center px-[140px] py-[98px] relative size-full" data-name="Sections" data-node-id="8671:26552">
+      <div className="absolute blur-[90.541px] filter h-[1248px] left-[94.53px] opacity-[0.03] translate-y-[-50%] w-[1250.94px]" data-node-id="8671:26553" style={{ top: "calc(50% + 0.191px)" }}>
+        <div className="absolute h-[1248px] left-0 top-0 w-[1250.94px]" data-node-id="8671:26554">
+          <div className="absolute inset-0" data-name="Vector" data-node-id="8671:26555">
+            <img alt="Vector" className="block max-w-none size-full" src={imgVector} />
+          </div>
+          <div className="absolute bottom-0 left-[33.65%] right-0 top-[33.73%]" data-name="Vector" data-node-id="8671:26556">
+            <img alt="Vector" className="block max-w-none size-full" height="827.094" src={imgVector1} width="830.038" />
+          </div>
+          <div className="absolute bottom-[36.56%] left-[67.19%] right-0 top-[33.68%]" data-node-id="8671:26557">
+            <img alt="Vector" className="block max-w-none size-full" src={imgVector2} />
+          </div>
+          <div className="absolute bottom-[33.72%] flex items-center justify-center left-0 right-[71.88%] top-[33.68%]">
+            <div className="flex-none h-[351.828px] rotate-[90deg] scale-y-[-100%] w-[406.861px]">
+              <div className="relative size-full" data-node-id="8671:26558">
+                <img alt="Vector" className="block max-w-none size-full" src={imgVector3} />
+              </div>
+            </div>
+          </div>
+          <div className="absolute bottom-0 left-[33.65%] right-0 top-[33.73%]" data-name="Vector" data-node-id="8671:26559">
+            <img alt="Vector" className="block max-w-none size-full" src={imgVector4} />
+          </div>
+          <div className="absolute bottom-[33.73%] left-0 right-[33.88%] top-0" data-name="Vector" data-node-id="8671:26560">
+            <img alt="Vector" className="block max-w-none size-full" height="827.094" src={imgVector5} width="827.094" />
+          </div>
+        </div>
+      </div>
+      <div className="absolute h-[516px] left-1/2 opacity-[0.02] translate-x-[-50%] translate-y-[-50%] w-[517.217px]" data-node-id="8671:26561" style={{ top: "calc(50% - 436px)" }}>
+        <div className="absolute h-[516px] left-0 top-0 w-[517.217px]" data-node-id="8671:26562">
+          <div className="absolute inset-0" data-name="Vector" data-node-id="8671:26563">
+            <img alt="Vector" className="block max-w-none size-full" src={imgVector6} />
+          </div>
+          <div className="absolute bottom-0 left-[33.65%] right-0 top-[33.73%]" data-name="Vector" data-node-id="8671:26564">
+            <img alt="Vector" className="block max-w-none size-full" height="341.972" src={imgVector7} width="343.189" />
+          </div>
+          <div className="absolute bottom-[36.56%] left-[67.19%] right-0 top-[33.68%]" data-node-id="8671:26565">
+            <img alt="Vector" className="block max-w-none size-full" src={imgVector8} />
+          </div>
+          <div className="absolute bottom-[33.72%] flex items-center justify-center left-0 right-[71.88%] top-[33.68%]">
+            <div className="flex-none h-[145.467px] rotate-[90deg] scale-y-[-100%] w-[168.222px]">
+              <div className="relative size-full" data-node-id="8671:26566">
+                <img alt="Vector" className="block max-w-none size-full" src={imgVector9} />
+              </div>
+            </div>
+          </div>
+          <div className="absolute bottom-0 left-[33.65%] right-0 top-[33.73%]" data-name="Vector" data-node-id="8671:26567">
+            <img alt="Vector" className="block max-w-none size-full" src={imgVector10} />
+          </div>
+          <div className="absolute bottom-[33.73%] left-0 right-[33.88%] top-0" data-name="Vector" data-node-id="8671:26568">
+            <img alt="Vector" className="block max-w-none size-full" height="341.972" src={imgVector11} width="341.972" />
+          </div>
+        </div>
+      </div>
+      <div className="content-stretch flex flex-col gap-3 items-center justify-end leading-[0] relative shrink-0 text-center" data-name="Tilte&Subtitle" data-node-id="8671:26569">
+        <div className="font-['Roboto:Medium',_sans-serif] font-medium relative shrink-0 text-[#7be385] text-[18px] tracking-[-0.3px] w-[760.5px]" data-node-id="8671:26570" style={{ fontVariationSettings: "'wdth' 100" }}>
+          <p className="leading-[1.65]">True Stories</p>
+        </div>
+        <div className="font-['Recoleta:Medium',_sans-serif] not-italic relative shrink-0 text-[56px] text-white tracking-[-1px] w-[942px]" data-node-id="8671:26571">
+          <p className="leading-[1.15]">Authentic Experiences from Our Esteemed Clients</p>
+        </div>
+      </div>
+      <div className="content-stretch flex flex-col gap-6 items-start justify-start relative shrink-0 w-full" data-name="Group clients'" data-node-id="8671:26572">
+        <div className="content-stretch flex gap-6 items-start justify-start relative shrink-0 w-full" data-name="card" data-node-id="8671:26573">
+          <div className="basis-0 box-border content-stretch flex flex-col gap-8 grow items-center justify-center min-h-px min-w-px p-[32px] relative rounded-[32px] shrink-0" data-name="Cards" data-node-id="8671:26574">
+            <div aria-hidden="true" className="absolute border-[#606162] border-[0.2px] border-solid inset-0 pointer-events-none rounded-[32px]" />
+            <div className="content-stretch flex flex-col gap-5 items-center justify-center relative shrink-0 w-full" data-name="User" data-node-id="8671:26575">
+              <div className="relative shrink-0 size-14" data-name="Image" data-node-id="8671:26576">
+                <img alt="Image" className="block max-w-none size-full" height="56" src={imgImage} width="56" />
+              </div>
+              <div className="content-stretch flex flex-col gap-2 items-center justify-center leading-[0] relative shrink-0 text-center text-nowrap w-full" data-name="text" data-node-id="8671:26577">
+                <div className="font-['Recoleta:SemiBold',_sans-serif] not-italic relative shrink-0 text-[#e6e7e7] text-[20px]" data-node-id="8671:26578">
+                  <p className="leading-[1.5] text-nowrap whitespace-pre">Jessica Tran</p>
+                </div>
+                <div className="font-['Roboto:Medium',_sans-serif] font-medium relative shrink-0 text-[#dadbdc] text-[14px]" data-node-id="8671:26579" style={{ fontVariationSettings: "'wdth' 100" }}>
+                  <p className="leading-[1.45] text-nowrap whitespace-pre">Product Manage, ReachGrow Agency</p>
+                </div>
+              </div>
+            </div>
+            <div className="font-['Roboto:Regular',_sans-serif] font-normal leading-[0] relative shrink-0 text-[#bec1c2] text-[18px] text-center tracking-[-0.3px] w-full" data-node-id="8671:26580" style={{ fontVariationSettings: "'wdth' 100" }}>
+              <p className="leading-[1.65]">This platform completely streamlined our workflows. It's user-friendly, saves us time, and keeps the entire team perfectly aligned every step. We can now focus on what truly matters and grow faster.</p>
+            </div>
+          </div>
+          <div className="basis-0 bg-[#122024] box-border content-stretch flex flex-col gap-8 grow items-center justify-center min-h-px min-w-px p-[32px] relative rounded-[32px] self-stretch shrink-0" data-name="Cards" data-node-id="8671:26581">
+            <div className="basis-0 content-stretch flex flex-col gap-5 grow items-center justify-center min-h-px min-w-px relative shrink-0" data-name="User" data-node-id="8671:26582">
+              <div className="relative shrink-0 size-[170.5px]" data-name="Image" data-node-id="8671:26583">
+                <img alt="Image" className="block max-w-none size-full" height="170.5" src={imgImage1} width="170.5" />
+              </div>
+              <div className="content-stretch flex flex-col gap-2 items-center justify-center leading-[0] relative shrink-0 text-center text-nowrap" data-name="text" data-node-id="8671:26584">
+                <div className="font-['Recoleta:SemiBold',_sans-serif] not-italic relative shrink-0 text-[#7be385] text-[24px] tracking-[-1px]" data-node-id="8671:26585">
+                  <p className="leading-[34px] text-nowrap whitespace-pre">Daniel Rodriguez</p>
+                </div>
+                <div className="font-['Roboto:Medium',_sans-serif] font-medium relative shrink-0 text-[#e6e7e7] text-[16px] tracking-[-0.3px]" data-node-id="8671:26586" style={{ fontVariationSettings: "'wdth' 100" }}>
+                  <p className="leading-[1.7] text-nowrap whitespace-pre">CEO, GrowthSync</p>
+                </div>
+                <div className="font-['Roboto:Medium',_sans-serif] font-medium relative shrink-0 text-[#e6e7e7] text-[16px] tracking-[-0.3px]" data-node-id="8671:26587" style={{ fontVariationSettings: "'wdth' 100" }}>
+                  <p className="leading-[1.7] text-nowrap whitespace-pre">Since 2005</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="basis-0 box-border content-stretch flex flex-col gap-8 grow items-center justify-center min-h-px min-w-px p-[32px] relative rounded-[32px] shrink-0" data-name="Cards" data-node-id="8671:26588">
+            <div aria-hidden="true" className="absolute border-[#606162] border-[0.2px] border-solid inset-0 pointer-events-none rounded-[32px]" />
+            <div className="content-stretch flex flex-col gap-5 items-center justify-center relative shrink-0 w-full" data-name="User" data-node-id="8671:26589">
+              <div className="relative shrink-0 size-14" data-name="Image" data-node-id="8671:26590">
+                <img alt="Image" className="block max-w-none size-full" height="56" src={imgImage2} width="56" />
+              </div>
+              <div className="content-stretch flex flex-col gap-2 items-center justify-center leading-[0] relative shrink-0 text-center text-nowrap w-full" data-name="text" data-node-id="8671:26591">
+                <div className="font-['Recoleta:SemiBold',_sans-serif] not-italic relative shrink-0 text-[#e6e7e7] text-[20px]" data-node-id="8671:26592">
+                  <p className="leading-[1.5] text-nowrap whitespace-pre">Laura Simmons</p>
+                </div>
+                <div className="font-['Roboto:Medium',_sans-serif] font-medium relative shrink-0 text-[#dadbdc] text-[14px]" data-node-id="8671:26593" style={{ fontVariationSettings: "'wdth' 100" }}>
+                  <p className="leading-[1.45] text-nowrap whitespace-pre">Operations Lead, InsightHub</p>
+                </div>
+              </div>
+            </div>
+            <div className="font-['Roboto:Regular',_sans-serif] font-normal leading-[0] relative shrink-0 text-[#bec1c2] text-[18px] text-center tracking-[-0.3px] w-full" data-node-id="8671:26594" style={{ fontVariationSettings: "'wdth' 100" }}>
+              <p className="leading-[1.65]">We've experienced a 40% increase in efficiency since adopting this tool. It's reliable, flexible, and absolutely essential for our operations. Our productivity has soared, and deadlines are consistently met with ease</p>
+            </div>
+          </div>
+        </div>
+        <div className="content-stretch flex gap-6 items-start justify-start relative shrink-0 w-full" data-name="card" data-node-id="8671:26595">
+          <div className="basis-0 box-border content-stretch flex flex-col gap-8 grow items-center justify-center min-h-px min-w-px p-[32px] relative rounded-[32px] shrink-0" data-name="Cards" data-node-id="8671:26596">
+            <div aria-hidden="true" className="absolute border-[#606162] border-[0.2px] border-solid inset-0 pointer-events-none rounded-[32px]" />
+            <div className="content-stretch flex flex-col gap-5 items-center justify-center relative shrink-0 w-full" data-name="User" data-node-id="8671:26597">
+              <div className="relative shrink-0 size-14" data-name="Image" data-node-id="8671:26598">
+                <img alt="Image" className="block max-w-none size-full" height="56" src={imgImage3} width="56" />
+              </div>
+              <div className="content-stretch flex flex-col gap-2 items-center justify-center leading-[0] relative shrink-0 text-center text-nowrap w-full" data-name="text" data-node-id="8671:26599">
+                <div className="font-['Recoleta:SemiBold',_sans-serif] not-italic relative shrink-0 text-[#e6e7e7] text-[20px]" data-node-id="8671:26600">
+                  <p className="leading-[1.5] text-nowrap whitespace-pre">Liam Chen</p>
+                </div>
+                <div className="font-['Roboto:Medium',_sans-serif] font-medium relative shrink-0 text-[#dadbdc] text-[14px]" data-node-id="8671:26601" style={{ fontVariationSettings: "'wdth' 100" }}>
+                  <p className="leading-[1.45] text-nowrap whitespace-pre">Founder , PeopleConnect</p>
+                </div>
+              </div>
+            </div>
+            <div className="font-['Roboto:Regular',_sans-serif] font-normal leading-[0] relative shrink-0 text-[#bec1c2] text-[18px] text-center tracking-[-0.3px] w-full" data-node-id="8671:26602" style={{ fontVariationSettings: "'wdth' 100" }}>
+              <p className="leading-[1.65]">The customization options are unmatched. It's like having a tailored solution that adapts seamlessly to every challenge our business faces. We've been able to create our own workflows with precision</p>
+            </div>
+          </div>
+          <div className="basis-0 box-border content-stretch flex flex-col gap-8 grow items-center justify-start min-h-px min-w-px p-[32px] relative rounded-[32px] self-stretch shrink-0" data-name="Cards" data-node-id="8671:26603">
+            <div aria-hidden="true" className="absolute border-[#606162] border-[0.2px] border-solid inset-0 pointer-events-none rounded-[32px]" />
+            <div className="basis-0 content-stretch flex flex-col gap-5 grow items-center justify-start min-h-px min-w-px relative shrink-0 w-full" data-name="User" data-node-id="8671:26604">
+              <div className="relative shrink-0 size-14" data-name="Image" data-node-id="8671:26605">
+                <img alt="Image" className="block max-w-none size-full" height="56" src={imgImage4} width="56" />
+              </div>
+              <div className="content-stretch flex flex-col gap-2 items-center justify-center leading-[0] relative shrink-0 text-center text-nowrap w-full" data-name="text" data-node-id="8671:26606">
+                <div className="font-['Recoleta:SemiBold',_sans-serif] not-italic relative shrink-0 text-[#e6e7e7] text-[20px]" data-node-id="8671:26607">
+                  <p className="leading-[1.5] text-nowrap whitespace-pre">Emma Davis</p>
+                </div>
+                <div className="font-['Roboto:Medium',_sans-serif] font-medium relative shrink-0 text-[#dadbdc] text-[14px]" data-node-id="8671:26608" style={{ fontVariationSettings: "'wdth' 100" }}>
+                  <p className="leading-[1.45] text-nowrap whitespace-pre">Data Analyst, AgileCore Technologies</p>
+                </div>
+              </div>
+            </div>
+            <div className="font-['Roboto:Regular',_sans-serif] font-normal leading-[0] relative shrink-0 text-[#bec1c2] text-[18px] text-center tracking-[-0.3px] w-full" data-node-id="8671:26609" style={{ fontVariationSettings: "'wdth' 100" }}>
+              <p className="leading-[1.65]">Analytics provided by this platform have been a total game-changer. It helps us make smarter, data-driven decisions daily. We now track key metrics more efficiently and improve strategies quickly</p>
+            </div>
+          </div>
+          <div className="basis-0 box-border content-stretch flex flex-col gap-8 grow items-center justify-center min-h-px min-w-px p-[32px] relative rounded-[32px] shrink-0" data-name="Cards" data-node-id="8671:26610">
+            <div aria-hidden="true" className="absolute border-[#606162] border-[0.2px] border-solid inset-0 pointer-events-none rounded-[32px]" />
+            <div className="content-stretch flex flex-col gap-5 items-center justify-center relative shrink-0 w-full" data-name="User" data-node-id="8671:26611">
+              <div className="relative shrink-0 size-14" data-name="Image" data-node-id="8671:26612">
+                <img alt="Image" className="block max-w-none size-full" height="56" src={imgImage5} width="56" />
+              </div>
+              <div className="content-stretch flex flex-col gap-2 items-center justify-center leading-[0] relative shrink-0 text-center text-nowrap w-full" data-name="text" data-node-id="8671:26613">
+                <div className="font-['Recoleta:SemiBold',_sans-serif] not-italic relative shrink-0 text-[#e6e7e7] text-[20px]" data-node-id="8671:26614">
+                  <p className="leading-[1.5] text-nowrap whitespace-pre">Sarah Lee</p>
+                </div>
+                <div className="font-['Roboto:Medium',_sans-serif] font-medium relative shrink-0 text-[#dadbdc] text-[14px]" data-node-id="8671:26615" style={{ fontVariationSettings: "'wdth' 100" }}>
+                  <p className="leading-[1.45] text-nowrap whitespace-pre">{`CEO ,  BuildSmart Co`}</p>
+                </div>
+              </div>
+            </div>
+            <div className="font-['Roboto:Regular',_sans-serif] font-normal leading-[0] relative shrink-0 text-[#bec1c2] text-[18px] text-center tracking-[-0.3px] w-full" data-node-id="8671:26616" style={{ fontVariationSettings: "'wdth' 100" }}>
+              <p className="leading-[1.65]">This tool is a must-have for every growing company. It's scalable, intuitive, and supports our rapid growth efforts seamlessly. We've been able to expand effortlessly while staying fully organized.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
