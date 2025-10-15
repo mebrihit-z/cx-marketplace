800e84c3f1320b7693a473236a75517a20fc2bb4
mebrihit-z <mebrihit@cxstudios.io>
2025-10-14 22:05:59 -0400

    adding more cards

--- src/data/templates.ts
+++ src/data/templates.ts
@@ -10,28 +10,28 @@ export interface MoreTemplate {
 }
 export const homePageTemplates: MoreTemplate[] = [
   {
-    id: "saas-1.0",
+    id: "horizon-1.0",
     tag: "SaaS",
     title: "Horizon",
-    backgroundImage: "images/saas-card-lg.svg",
+    backgroundImage: "images/horizon-card-lg.svg",
     backgroundColor: "#4DD58C",
     gradientTo: "#04261E",
     aspectRatio: "aspect-[1860/1212]"
   },
   {
-    id: "e-commerce-1.0",
+    id: "agora-1.0",
     tag: "E-commerce",
     title: "Agora",
-    backgroundImage: "images/e-commerce-card-lg.svg",
+    backgroundImage: "images/agora-card-lg.svg",
     backgroundColor: "#6D83D9",
     gradientTo: "#131D45",
     aspectRatio: "aspect-[1726/1124]"
   },
   {
-    id: "startup-1.0",
+    id: "nova-1.0",
     tag: "Startup",
     title: "Nova",
-    backgroundImage: "images/startup-card-lg.svg",
+    backgroundImage: "images/nova-card-lg.svg",
     backgroundColor: "#BD550F",
     gradientTo: "#421E06",
     aspectRatio: "aspect-[1666/1084]"
@@ -42,7 +42,7 @@ export const galleryTemplates: MoreTemplate[] = [
     id: "saas-1.0",
     tag: "SaaS",
     title: "Horizon",
-    backgroundImage: "images/saas-card-lg.svg",
+    backgroundImage: "images/horizon-card-lg.svg",
     backgroundColor: "#4DD58C",
     gradientTo: "#04261E",
     aspectRatio: "aspect-[1860/1212]"
@@ -51,7 +51,7 @@ export const galleryTemplates: MoreTemplate[] = [
     id: "e-commerce-1.0",
     tag: "E-commerce",
     title: "Agora",
-    backgroundImage: "images/e-commerce-card-lg.svg",
+    backgroundImage: "images/agora-card-lg.svg",
     backgroundColor: "#6D83D9",
     gradientTo: "#131D45",
     aspectRatio: "aspect-[1726/1124]"
@@ -60,7 +60,7 @@ export const galleryTemplates: MoreTemplate[] = [
     id: "startup-1.0",
     tag: "Startup",
     title: "Nova",
-    backgroundImage: "images/startup-card-lg.svg",
+    backgroundImage: "images/nova-card-lg.svg",
     backgroundColor: "#BD550F",
     gradientTo: "#421E06",
     aspectRatio: "aspect-[1666/1084]"
@@ -69,7 +69,7 @@ export const galleryTemplates: MoreTemplate[] = [
     id: "startup-11",
     tag: "Portfolio",
     title: "Canvas",
-    backgroundImage: "images/portfolio-card-lg.svg",
+    backgroundImage: "images/canvas-card-lg.svg",
     backgroundColor: "#E9D101",
     gradientTo: "#4D4000",
     aspectRatio: "aspect-[1666/1084]"
@@ -78,7 +78,7 @@ export const galleryTemplates: MoreTemplate[] = [
     id: "startup-11",
     tag: "Event",
     title: "Pulse",
-    backgroundImage: "images/event-card-lg.svg",
+    backgroundImage: "images/pulse-card-lg.svg",
     backgroundColor: "#8B0B68",
     gradientTo: "#2C0321",
     aspectRatio: "aspect-[1666/1084]"
@@ -87,15 +87,89 @@ export const galleryTemplates: MoreTemplate[] = [
     id: "startup-11",
     tag: "Agency",
     title: "Mosaic",
-    backgroundImage: "images/agency-card-lg.svg",
+    backgroundImage: "images/mosaic-card-lg.svg",
     backgroundColor: "#0F1AAA",
     gradientTo: "#030529",
     aspectRatio: "aspect-[1666/1084]"
   }
 ];
+export const galleryMoreTemplates: MoreTemplate[] = [
+  {
+    id: "harbor-1.0",
+    tag: "E-commerce",
+    title: "Harbor",
+    backgroundImage: "images/harbor-card-lg.svg",
+    backgroundColor: "#5A581C",
+    gradientTo: "#1D1C04",
+    aspectRatio: "aspect-[1860/1212]"
+  },
+  {
+    id: "prism-1.0",
+    tag: "Portfolio",
+    title: "Prism",
+    backgroundImage: "images/prism-card-lg.svg",
+    backgroundColor: "#7C33E2",
+    gradientTo: "#260B41",
+    aspectRatio: "aspect-[1726/1124]"
+  },
+  {
+    id: "atlas-1.0",
+    tag: "News",
+    title: "Atlas",
+    backgroundImage: "images/atlas-card-lg.svg",
+    backgroundColor: "#8FA2A3",
+    gradientTo: "#283536",
+    aspectRatio: "aspect-[1666/1084]"
+  },
+  {
+    id: "spark-1.0",
+    tag: "Startup",
+    title: "Spark",
+    backgroundImage: "images/spark-card-lg.svg",
+    backgroundColor: "#42AB18",
+    gradientTo: "#0A2B07",
+    aspectRatio: "aspect-[1666/1084]"
+  },
+  {
+    id: "nexus-1.0",
+    tag: "SaaS",
+    title: "Nexus",
+    backgroundImage: "images/nexus-card-lg.svg",
+    backgroundColor: "#236E84",
+    gradientTo: "#032636",
+    aspectRatio: "aspect-[1666/1084]"
+  },
+  {
+    id: "summit-1.0",
+    tag: "Business",
+    title: "Summit",
+    backgroundImage: "images/summit-card-lg.svg",
+    backgroundColor: "#6E0D0D",
+    gradientTo: "#2C0321",
+    aspectRatio: "aspect-[1666/1084]"
+  },
+  {
+    id: "verse-1.0",
+    tag: "Blog",
+    title: "Verse",
+    backgroundImage: "images/verse-card-lg.svg",
+    backgroundColor: "#C03C1B",
+    gradientTo: "#2C0321",
+    aspectRatio: "aspect-[1666/1084]"
+  },
+  {
+    id: "chroma-1.0",
+    tag: "Agency",
+    title: "Chroma",
+    backgroundImage: "images/chroma-card-lg.svg",
+    backgroundColor: "#27274E",
+    gradientTo: "#030316",
+    aspectRatio: "aspect-[1666/1084]"
+  }
+];
 export const moreTemplates: MoreTemplate[] = [
   {
-    id: "saas-1.0",
+    id: "nexus-1.0",
     tag: "SaaS",
     title: "Nexus",
     backgroundImage: "images/nexus-card-lg.svg",
@@ -104,16 +178,16 @@ export const moreTemplates: MoreTemplate[] = [
     aspectRatio: "aspect-[1860/1212]"
   },
   {
-    id: "startup-1.0",
+    id: "nova-1.0",
     tag: "Startup",
     title: "Nova",
-    backgroundImage: "images/startup-card-lg.svg",
+    backgroundImage: "images/nova-card-lg.svg",
     backgroundColor: "#BD550F",
     gradientTo: "#421E06",
     aspectRatio: "aspect-[1666/1084]"
   },
   {
-    id: "e-commerce-1.0",
+    id: "spark-1.0",
     tag: "Startup",
     title: "Spark",
     backgroundImage: "images/spark-card-lg.svg",
