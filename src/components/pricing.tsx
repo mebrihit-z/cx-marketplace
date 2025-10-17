a3f34907f7ff37e574eaa34a1f698b8e90e4f2d3
mebrihit-z <mebrihit@cxstudios.io>
2025-10-17 13:48:26 -0400

    pricing bg changes

--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -20,8 +20,9 @@ function Button({ text = "Button", state = "Default", buttonType = "Default" }: 
 }
 
 // Image assets
-const imgImage19 = "images/pro-card-bg-image.svg";
-const greenCardBgImage = "images/premium-card-bg-image.svg";
+const imgBasicCardBgImage = "images/basic-card-bg-image.svg";
+const imgProCardBgImage = "images/pro-card-bg-image.svg";
+const imgPremiumCardBgImage = "images/premium-card-bg-image.svg";
 const whiteCheckIcon = "icons/white-check-icon.svg";
 const imgInfo = "icons/info-icon.svg";
 const imgMaterialSymbolsAttachMoneyRounded = "icons/money-icon.svg";
@@ -52,8 +53,7 @@ export default function Pricing() {
         <div className="box-border content-stretch flex flex-col gap-12 items-center justify-start overflow-clip px-4 sm:px-8 lg:px-32 py-0 relative shrink-0 w-full" data-name="Container" data-node-id="8272:91887">
           <div className="content-stretch flex flex-col lg:flex-row gap-12 items-start justify-center relative shrink-0 w-full" data-name="Pricing Cards" data-node-id="8272:96386">
             {/* Basic Plan */}
-            <div className="bg-[#032900] box-border content-stretch flex flex-col gap-6 items-start justify-start overflow-clip px-8 py-10 relative rounded-[16px] shrink-0 w-full lg:w-[400px]" data-name="_Pricing tier card" data-node-id="8272:92105">
-              <div className="absolute bg-gradient-to-b from-[#569800] left-1/2 opacity-40 to-[#00a748] top-1/2 translate-x-[-50%] translate-y-[-50%] via-50% via-[#e7ffc9] w-full lg:w-[400px]" data-name="Background" data-node-id="8272:92257" />
+            <div className="bg-white box-border content-stretch flex flex-col gap-6 items-start justify-start overflow-clip px-8 py-10 relative rounded-[16px] shrink-0 w-full lg:w-[400px]" data-name="_Pricing tier card" data-node-id="8272:92105" style={{ top: "calc(50% - 0.5px)", backgroundImage: `url('${imgBasicCardBgImage}')` }}>
               <div className="font-['Inter:Semi_Bold',_sans-serif] font-semibold leading-[0] min-w-full not-italic relative shrink-0 text-[40px] text-white" data-node-id="8272:92107" style={{ width: "min-content" }}>
                 <p className="leading-[48px]">Basic</p>
               </div>
@@ -119,7 +119,7 @@ export default function Pricing() {
             </div>
 
             {/* Pro Plan */}
-               <div className="bg-white box-border content-stretch flex flex-col gap-6 items-start justify-start overflow-clip px-8 py-10 relative rounded-[16px] shrink-0 w-full lg:w-[400px] " data-name="_Pricing tier card" data-node-id="8272:92161" style={{ top: "calc(50% - 0.5px)", backgroundImage: `url('${imgImage19}')` }}>
+               <div className="bg-white box-border content-stretch flex flex-col gap-6 items-start justify-start overflow-clip px-8 py-10 relative rounded-[16px] shrink-0 w-full lg:w-[400px] " data-name="_Pricing tier card" data-node-id="8272:92161" style={{ top: "calc(50% - 0.5px)", backgroundImage: `url('${imgProCardBgImage}')` }}>
               
                 <div className="font-['Inter:Semi_Bold',_sans-serif] font-semibold leading-[0] min-w-full not-italic relative shrink-0 text-[40px] text-white" data-node-id="8272:92219" style={{ width: "min-content" }}> 
                   <p className="leading-[48px]">Pro</p>
@@ -201,7 +201,7 @@ export default function Pricing() {
                 </div>
               </div>
             {/* Premium Plan */}
-            <div className="bg-white box-border content-stretch flex flex-col gap-6 items-start justify-start overflow-clip px-8 py-10 relative rounded-[16px] shrink-0 w-full lg:w-[400px]" data-name="_Pricing tier card" data-node-id="8272:92161" style={{ top: "calc(50% - 0.5px)", backgroundImage: `url('${greenCardBgImage}')` }}>
+            <div className="bg-white box-border content-stretch flex flex-col gap-6 items-start justify-start overflow-clip px-8 py-10 relative rounded-[16px] shrink-0 w-full lg:w-[400px]" data-name="_Pricing tier card" data-node-id="8272:92161" style={{ top: "calc(50% - 0.5px)", backgroundImage: `url('${imgPremiumCardBgImage}')` }}>
               <div className="font-['Inter:Semi_Bold',_sans-serif] font-semibold leading-[0] min-w-full not-italic relative shrink-0 text-[40px] text-white" data-node-id="8272:92163" style={{ width: "min-content" }}>
                 <p className="leading-[48px]">Premium</p>
               </div>
